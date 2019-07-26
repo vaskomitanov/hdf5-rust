6e31c00d7eac9e8bd4984fc51c9afe711c273e9e
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-07-26 01:06:56 +0100

    LZF: add feature = "lzf", build liblzf

--- build.rs
+++ build.rs
@@ -1,3 +1,16 @@
+#[cfg(feature = "lzf")]
+fn build_lzf() {
+    cc::Build::new()
+        .warnings(false)
+        .opt_level(3)
+        .file("ext/lzf/lzf_c.c")
+        .file("ext/lzf/lzf_d.c")
+        .include("ext/lzf")
+        .compile("lzf");
+}
+
 fn main() {
     hdf5_sys::emit_cfg_flags();
+    #[cfg(feature = "lzf")]
+    build_lzf();
 }
